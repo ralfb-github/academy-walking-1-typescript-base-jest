daf8c34a8c5f364b606139c1ccd1a568a57d4900
Ralf Braitling <ralf.braitling@cluno.com>
2022-01-27 17:43:44 +0100

    add printermock

--- src/main/bankkata.ts
+++ src/main/bankkata.ts
@@ -1,20 +1,41 @@
+
+
 interface IAccountService{
     deposit(amount:number):void;
     withdraw(amount:number):void;
-    printStatement(amount:number):void;
+    printStatement():void;
+}
+
+export interface IPrinter {
+    print(row:string):void
+}
+export interface IDepositService {
+    deposit(date:string, anount:number):void
 }
 
 export class AccountService implements IAccountService {
 
-    
+    balance: number;
+    rows:string;
+    printer: IPrinter;
+
+    constructor(printer: IPrinter){
+        this.balance = 0;
+        this.printer=printer;
+        this.rows = "Date||Amount||Balance";
+    }
+
     public deposit(amount: number): void {
-        
-    }
-    public withdraw(amount: number): void {
-        
-    }
-    public printStatement(amount: number): void {
-        
-    }
+       this.balance = this.balance + amount; 
+       this.rows += `+14/09/2021||${amount}||${this.balance}`
  
+    }
+
+    public withdraw(amount: number): void {
+       this.balance = this.balance - amount;     
+    }
+
+    public printStatement(): void {
+        this.printer.print(this.rows)        
+    }
 }
--- src/test/bankkata.test.ts
+++ src/test/bankkata.test.ts
@@ -1,9 +1,32 @@
-import {AccountService} from "../main/bankkata"
+import {AccountService, IPrinter} from "../main/bankkata"
+import {mock} from 'jest-mock-extended'
+
 describe('test AccountService', () => {
     
     it("accountService is initialized", () => {
-        const accountService = new AccountService()
+        let printerMock = mock<IPrinter>();
+        const accountService = new AccountService(printerMock)
         expect(accountService).toBeDefined();
+    }),
+
+    it("should print a empty account statement", () => {
+        let printerMock = mock<IPrinter>();
+        const accountService = new AccountService(printerMock);
+        accountService.printStatement();
+        expect(printerMock.print).toBeCalledWith("Date||Amount||Balance");
+    }),
+
+    it("should print the first row", () => {
+        let printerMock = mock<IPrinter>();
+        const accountService = new AccountService(printerMock);
+        accountService.deposit(1000)
+        accountService.printStatement();
+        expect(printerMock.print).toBeCalledWith("Date||Amount||Balance+14/09/2021||1000||1000");
     })
+
+/*     Given a client makes a deposit of 1000 on 10-01-2012
+    And a deposit of 2000 on 13-01-2012
+    And a withdrawal of 500 on 14-01-2012
+    When they print their bank statement */
 }
 )
