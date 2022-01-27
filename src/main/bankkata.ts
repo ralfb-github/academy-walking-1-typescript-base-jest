7b6a5dd61fa7e60019a9cca397599234025c8a5a
Nick Roberts <nick.roberts@smartfleetsolutions.com>
2022-01-27 16:03:55 +0000

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/bankkata.ts
+++ src/main/bankkata.ts
@@ -1,20 +1,33 @@
 interface IAccountService{
     deposit(amount:number):void;
     withdraw(amount:number):void;
-    printStatement(amount:number):void;
+    printStatement():void;
+}
+
+export interface IPrinter {
+    print(row:string):void
 }
 
 export class AccountService implements IAccountService {
 
-    
+    balance: number;
+    printer: IPrinter;
+
+    constructor(printer: IPrinter){
+        this.balance = 0;
+        this.printer = printer;
+    }
+
     public deposit(amount: number): void {
-        
+       this.balance = this.balance + amount;  
     }
+
     public withdraw(amount: number): void {
-        
+       this.balance = this.balance - amount;     
     }
-    public printStatement(amount: number): void {
-        
+
+    public printStatement(): void {
+        this.printer.print("Date||Amount||Balance")        
     }
- 
+     
 }
--- src/test/bankkata.test.ts
+++ src/test/bankkata.test.ts
@@ -1,9 +1,29 @@
-import {AccountService} from "../main/bankkata"
+import {AccountService, IPrinter} from "../main/bankkata"
+import {mock} from 'jest-mock-extended'
+
 describe('test AccountService', () => {
     
     it("accountService is initialized", () => {
-        const accountService = new AccountService()
+        let printerMock = mock<IPrinter>();
+        const accountService = new AccountService(printerMock)
         expect(accountService).toBeDefined();
+    }),
+
+    it("should print a empty account statement", () => {
+        let printerMock = mock<IPrinter>();
+        const accountService = new AccountService(printerMock);
+        expect(printerMock.print).toBeCalledWith("Date||Amount||Balance");
+    }),
+
+    it("should start with a balance of zero", () => {
+        let printerMock = mock<IPrinter>();
+        const accountService = new AccountService(printerMock)
+        expect(accountService.balance).toBe(0);
     })
+
+/*     Given a client makes a deposit of 1000 on 10-01-2012
+    And a deposit of 2000 on 13-01-2012
+    And a withdrawal of 500 on 14-01-2012
+    When they print their bank statement */
 }
 )
